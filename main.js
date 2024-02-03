365539fe79f5c912a87547c0966d8c6aa09cea42
gdnoob1324 <gdnoob1324@gmail.com>
2024-02-03 14:45:24 +0900

    u

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'http://w.soundcloud.com/player/api.js';
-document.head.appendChild(script);
-
 function setVolume() {
     var widgetIframe = document.getElementById("scplayer"),
         fixWidget = SC.Widget(widgetIframe);
