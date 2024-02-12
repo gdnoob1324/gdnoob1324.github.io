fd301ecd7a8dacf2d765010d8639a02dcb745dce
gdnoob1324 <gdnoob1324@gmail.com>
2024-02-12 21:19:19 +0900

    u

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function setVolume() {
 }
 
 $("*").click(function () {
-    var widgetIframe = document.getElementById("scplayer"),
-        fixWidget = SC.Widget(widgetIframe);
-    fixWidget.setVolume(20);
+    $("*").click(function () {
+        setVolume();
+    });
 });
