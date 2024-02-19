4693152752b54d3e07dd742ede7db312aec9a7dc
gdnoob1324 <gdnoob1324@gmail.com>
2024-02-19 14:21:26 +0900

    u

--- main.js
+++ main.js
@@ -11,3 +11,17 @@
 // setTimeout(function () {
 //     setVolume();
 // }, 1000);
+
+let currentOrientation = function () {
+    if (screen.availHeight > screen.availWidth) {
+        //	 세로모드일 때 실행할 이벤트
+        $("html").addClass('portrait').removeClass('landscape');
+    }
+    else {
+        //	가로모드일 때 실행할 이벤트
+        $("html").addClass('landscape').removeClass('portrait');
+    }
+}
+
+currentOrientation();
+window.addEventListener("resize", currentOrientation);
