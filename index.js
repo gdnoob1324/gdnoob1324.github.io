ea2eed873122f6a942537c3baa5f7311f9a4ebb7
gdnoob1324 <gdnoob1324@gmail.com>
2024-03-13 22:26:11 +0900

    u

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('yt-player', {
         videoId: ytId,
         events: {
-            'onReady': ()=>{
+            'onReady': () => {
                 // event.target.playVideo();
             },
-            'onStateChange': ()=>{
+            'onStateChange': () => {
             }
         }
     });
 }
 
 function changeVideo(id) {
-  let videoId = '';
-  let button = $('.controller>button');
-  switch(id) {
-    case 0: videoId = 'oCrobJMZBTo'; break;
-    case 1: videoId = 'YCg__giT4ko'; break;
-    case 2: videoId = 'RBIUyqNBnZs'; break;
-  }
-  button.removeClass("focus");
-  button.eq(id).addClass("focus");
-  player.loadVideoById(videoId);
+    let videoId = '';
+    let button = $('.controller>button');
+    switch (id) {
+        case 0: videoId = 'oCrobJMZBTo'; break;
+        case 1: videoId = 'YCg__giT4ko'; break;
+        case 2: videoId = 'RBIUyqNBnZs'; break;
+    }
+    button.removeClass("focus");
+    button.eq(id).addClass("focus");
+    player.loadVideoById(videoId);
 }
