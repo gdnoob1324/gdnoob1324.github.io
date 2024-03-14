7617b6944e481a0d2ce1086a2d19c80b45dfb67e
gdnoob1324 <gdnoob1324@gmail.com>
2024-03-14 17:38:33 +0900

    u

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// var tag = document.createElement('script');
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('yt-player', {
