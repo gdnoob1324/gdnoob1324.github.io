77ae3b8b6e3bf90a25d1355ada8742b1b12e3312
gdnoob1324 <gdnoob1324@gmail.com>
2024-03-21 19:59:53 +0900

    u

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var gid = Number(((new URL(window.location)).searchParams).get('tab'));
 
 function getV(n) {
     switch (n) {
-        case 0: return 'kXF3VYYa5TI'; // oCrobJMZBTo
+        case 0: return 'oCrobJMZBTo';
         case 1: return '29N8_pSsWCE';
         case 2: return 'skDHFDdRH5s';
     }
