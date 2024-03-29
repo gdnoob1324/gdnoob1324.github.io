677298b7ae9b8c7ed31c6d6366dadd65626d7ab1
gdnoob1324 <gdnoob1324@gmail.com>
2024-03-29 19:11:17 +0900

    u

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (getBrowser().toLowerCase() == 'naver') {
 }
 
 var tag = document.createElement('script');
-tag.src = "https:www.youtube.com/iframe_api";
+tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
