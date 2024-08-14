9491ab613361084d39eedd6ce26d6c6e6eda10e2
gdnoob1324 <gdnoob1324@gmail.com>
2024-08-14 21:33:49 +0900

    u

---
+++ index.js
@@ -0,0 +1,5 @@
+function ToggleSide() {
+    if (document.getElementById('Sidebar').style.display == 'none')
+        document.getElementById('Sidebar').style.display = 'block';
+    else document.getElementById('Sidebar').style.display = 'none';
+}
