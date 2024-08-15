27b97970edc84723ee07d7f6fc4832afd8ca766c
gdnoob1324 <gdnoob1324@gmail.com>
2024-08-15 18:06:16 +0900

    u

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ function ToggleSide() {
         document.getElementById('Sidebar').style.display = 'block';
     else document.getElementById('Sidebar').style.display = 'none';
 }
+
+
+var NaverSvg = 'data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIxOCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCiAgICA8cGF0aCBkPSJNMTYuMjczIDEyLjg0NSA3LjM3NiAwSDB2MjRoNy43MjZWMTEuMTU2TDE2LjYyNCAyNEgyNFYwaC03LjcyN3YxMi44NDVaIiBmaWxsPSIjMDNDNzVBIj48L3BhdGg+DQo8L3N2Zz4='
+var GoogleSvg = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjIwIj4KICA8cGF0aCBkPSJNMjIuNTYgMTIuMjVjMC0uNzgtLjA3LTEuNTMtLjItMi4yNUgxMnY0LjI2aDUuOTJjLS4yNiAxLjM3LTEuMDQgMi41My0yLjIxIDMuMzF2Mi43N2gzLjU3YzIuMDgtMS45MiAzLjI4LTQuNzQgMy4yOC04LjA5eiIgZmlsbD0iIzQyODVGNCIvPjxwYXRoIGQ9Ik0xMiAyM2MyLjk3IDAgNS40Ni0uOTggNy4yOC0yLjY2bC0zLjU3LTIuNzdjLS45OC42Ni0yLjIzIDEuMDYtMy43MSAxLjA2LTIuODYgMC01LjI5LTEuOTMtNi4xNi00LjUzSDIuMTh2Mi44NEMzLjk5IDIwLjUzIDcuNyAyMyAxMiAyM3oiIGZpbGw9IiMzNEE4NTMiLz48cGF0aCBkPSJNNS44NCAxNC4wOWMtLjIyLS42Ni0uMzUtMS4zNi0uMzUtMi4wOXMuMTMtMS40My4zNS0yLjA5VjcuMDdIMi4xOEMxLjQzIDguNTUgMSAxMC4yMiAxIDEycy40MyAzLjQ1IDEuMTggNC45M2wyLjg1LTIuMjIuODEtLjYyeiIgZmlsbD0iI0ZCQkMwNSIvPjxwYXRoIGQ9Ik0xMiA1LjM4YzEuNjIgMCAzLjA2LjU2IDQuMjEgMS42NGwzLjE1LTMuMTVDMTcuNDUgMi4wOSAxNC45NyAxIDEyIDEgNy43IDEgMy45OSAzLjQ3IDIuMTggNy4wN2wzLjY2IDIuODRjLjg3LTIuNiAzLjMtNC41MyA2LjE2LTQuNTN6IiBmaWxsPSIjRUE0MzM1Ii8+PHBhdGggZD0iTTEgMWgyMnYyMkgxeiIgZmlsbD0ibm9uZSIvPgo8L3N2Zz4='
+function switchQ() {
+    let form = document.getElementById('qform');
+    let input = document.getElementById('qinput');
+    let img = document.getElementById('qimg');
+    input.name = input.name == 'query' ? 'q' : 'query';
+    form.action = form.action == input.name == 'q' ? 'https://www.google.com/search' : 'https://m.search.naver.com/search.naver';
+    if (input.name == 'query') img.src = NaverSvg;
+    else  img.src = GoogleSvg;
+}
