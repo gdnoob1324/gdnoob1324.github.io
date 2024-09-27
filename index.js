5e4533b6dce3480284aba9ac9abd800f5a5db531
gdnoob1324 <gdnoob1324@gmail.com>
2024-09-27 20:58:00 +0900

    u

---
+++ index.js
@@ -0,0 +1,40 @@
+function setDivAHeight() {
+    document.documentElement.style.setProperty('--header-height', document.querySelector('.header').offsetHeight + 'px');
+}
+window.onload = setDivAHeight;
+window.addEventListener('resize', setDivAHeight);
+
+window.onload = function() {
+    const scrollableElement = document.querySelector('.slides');
+    scrollableElement.addEventListener('wheel', (event) => {
+        event.preventDefault();
+        scrollableElement.scrollLeft += event.deltaY;
+    });
+    
+    const scrollable = scrollableElement;
+    let isDragging = false;
+    let startX;
+    
+    scrollable.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        startX = e.clientX;
+    });
+    
+    scrollable.addEventListener('mousemove', (e) => {
+        if (isDragging) {
+            const deltaX = e.clientX - startX;
+            scrollable.scrollLeft -= deltaX;
+            startX = e.clientX;
+        }
+    });
+    
+    scrollable.addEventListener('mouseup', () => {
+        isDragging = false;
+    });
+
+    scrollable.addEventListener('mouseleave', () => {
+        isDragging = false;
+    });
+
+
+};
