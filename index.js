10488cae36889fe8ee80af38f433a9288a322e4f
gdnoob1324 <gdnoob1324@gmail.com>
2024-09-28 11:25:37 +0900

    u

--- index.js
+++ index.js
@@ -10,29 +10,4 @@ window.onload = function() {
         event.preventDefault();
         scrollableElement.scrollLeft += event.deltaY;
     });
-    
-    // const scrollable = scrollableElement;
-    // let isDragging = false;
-    // let startX;
-    
-    // scrollable.addEventListener('mousedown', (e) => {
-    //     isDragging = true;
-    //     startX = e.clientX;
-    // });
-    
-    // scrollable.addEventListener('mousemove', (e) => {
-    //     if (isDragging) {
-    //         const deltaX = e.clientX - startX;
-    //         scrollable.scrollLeft -= deltaX;
-    //         startX = e.clientX;
-    //     }
-    // });
-    
-    // scrollable.addEventListener('mouseup', () => {
-    //     isDragging = false;
-    // });
-
-    // scrollable.addEventListener('mouseleave', () => {
-    //     isDragging = false;
-    // });
 };
