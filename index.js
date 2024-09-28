4d69e3b330623bc88ac0f9acec1686743db649ad
gdnoob1324 <gdnoob1324@gmail.com>
2024-09-28 11:24:41 +0900

    u

--- index.js
+++ index.js
@@ -11,28 +11,28 @@ window.onload = function() {
         scrollableElement.scrollLeft += event.deltaY;
     });
     
-    const scrollable = scrollableElement;
-    let isDragging = false;
-    let startX;
+    // const scrollable = scrollableElement;
+    // let isDragging = false;
+    // let startX;
     
-    scrollable.addEventListener('mousedown', (e) => {
-        isDragging = true;
-        startX = e.clientX;
-    });
+    // scrollable.addEventListener('mousedown', (e) => {
+    //     isDragging = true;
+    //     startX = e.clientX;
+    // });
     
-    scrollable.addEventListener('mousemove', (e) => {
-        if (isDragging) {
-            const deltaX = e.clientX - startX;
-            scrollable.scrollLeft -= deltaX;
-            startX = e.clientX;
-        }
-    });
+    // scrollable.addEventListener('mousemove', (e) => {
+    //     if (isDragging) {
+    //         const deltaX = e.clientX - startX;
+    //         scrollable.scrollLeft -= deltaX;
+    //         startX = e.clientX;
+    //     }
+    // });
     
-    scrollable.addEventListener('mouseup', () => {
-        isDragging = false;
-    });
+    // scrollable.addEventListener('mouseup', () => {
+    //     isDragging = false;
+    // });
 
-    scrollable.addEventListener('mouseleave', () => {
-        isDragging = false;
-    });
+    // scrollable.addEventListener('mouseleave', () => {
+    //     isDragging = false;
+    // });
 };
